8b1cf172d469ef62a281fbd5825f7b0abb65d179
Alyasa62 <m.alyasa62@gmail.com>
2025-03-29 20:00:23 +0500

    nvm

--- lib/pages/timer_page.dart
+++ lib/pages/timer_page.dart
@@ -11,6 +11,7 @@ class TimerPage extends StatefulWidget {
   const TimerPage({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _TimerPageState createState() => _TimerPageState();
 }
 
